e0c2b43ed925d03c9aa281db4ee29b536a8f60ff
Crescens <2e3crescens@gmail.com>
2024-01-11 22:46:49 +0800

    Updated books.js from native HTML to a React Component

--- books.js
+++ books.js
@@ -1,43 +1,34 @@
-class Book extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  set book(book) {
-    this.innerHTML = `
-          <div class="card">
-              <h5 class="card-header">${book.title}</h5>
-              <div class="card-body">
-                  <h5>${book.subtitle}</h5>
-                  <p class="card-text">
-                      <table class="table">
-                          <tr>
-                              <td class="text-success font-weight-bold">Title:</td>
-                              <td>${book.title}</td>
-                          </tr>
-                          <tr>
-                              <td class="text-success font-weight-bold">Subtitle:</td>
-                              <td>${book.subtitle}</td>
-                          </tr>
-                          <tr>
-                              <td class="text-success font-weight-bold">Author:</td>
-                              <td>${book.author}</td>
-                          </tr>
-                          <tr>
-                              <td class="text-success font-weight-bold">Publisher:</td>
-                              <td>${book.publisher}</td>
-                          </tr>
-                          <tr>
-                              <td class="text-success font-weight-bold">Description:</td>
-                              <td>${book.description}</td>
-                          </tr>
-                      </table>
-                  </p>
-              </div>
-          </div>
-      `;
-  }
+function Book() {
+  return (
+    <div class="card">
+      <h5 class="card-header">{data.title}</h5>
+      <div class="card-body">
+        <h5>${data.subtitle}</h5>
+        <p class="card-text">
+          <table class="table">
+            <tr>
+              <td class="text-success font-weight-bold">Title:</td>
+              <td>${data.title}</td>
+            </tr>
+            <tr>
+              <td class="text-success font-weight-bold">Subtitle:</td>
+              <td>${data.subtitle}</td>
+            </tr>
+            <tr>
+              <td class="text-success font-weight-bold">Author:</td>
+              <td>${data.author}</td>
+            </tr>
+            <tr>
+              <td class="text-success font-weight-bold">Publisher:</td>
+              <td>${data.publisher}</td>
+            </tr>
+            <tr>
+              <td class="text-success font-weight-bold">Description:</td>
+              <td>${data.description}</td>
+            </tr>
+          </table>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-// Define the elements
-customElements.define("my-book", Book);
